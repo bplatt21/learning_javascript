400d0a2a589d519951df7e6fdfb3608c5d9d1fc0
bplatt21 <bplatt21@gmail.com>
2015-12-19 13:38:27 -0500

    Added learning_javascript.js

---
+++ learning_javascript.js
@@ -0,0 +1,15 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+var sleepCheck = function (numHours) {
+    if (numHours >= 8) {
+        return "You're getting plenty of sleep! Maybe even too much!";
+    }
+    else {
+        return "Get some more shut eye";
+        } 
+        }
+        console.log(sleepCheck (9))
+        console.log(sleepCheck (11))
+        console.log(sleepCheck (7))
+
+        </SCRIPT>
